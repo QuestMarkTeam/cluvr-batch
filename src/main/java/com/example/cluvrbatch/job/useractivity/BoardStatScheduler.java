22fb79ec2c593d3041fb23be3870b5aaa0a80308
dnjs5024 <dnjs7469@naver.com>
2025-06-25 00:40:22 +0900

    Feat:#27 제발 말해줘말해줘 나 살수있게

--- src/main/java/com/example/cluvrbatch/job/useractivity/BoardStatScheduler.java
+++ src/main/java/com/example/cluvrbatch/job/useractivity/BoardStatScheduler.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -17,14 +19,22 @@ public class BoardStatScheduler {
 	private final Job boardLogJob;
 	private final BoardStatJobService boardStatJobService;
 
-	@Scheduled(cron = "0 * * * * *") // 매일 새벽 3시
+	@Autowired
+	private StringRedisTemplate stringRedisTemplate;
+
+	@Scheduled(cron = "0 * * * * *") // 매 분 0초마다 실행
 	public void runBoardStatJob() {
 		try {
-			log.info("batch 스캐쥴 잘 실행 중 0620 2210수정");
+			log.info("batch 스캐쥴 잘 실행 중 0625 제발 지옥같은 여기서 날 꺼내줘 수정");
+
+			// Redis 테스트 저장
+			stringRedisTemplate.opsForValue().set("batch:ping", "pong");
+
 			boardStatJobService.runJob();
 		} catch (Exception e) {
-			// 로깅 or 슬랙 알림
-			throw new IllegalStateException("runBoardStatJob 실행 실패", e);
+			throw new IllegalStateException("runBoardStatJob 실행 실패 이게 꿈이라면 어서 날 깨워줘 모든 것이 거짓말이라고 해줘", e);
 		}
 	}
+
+
 }
